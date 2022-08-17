21cb5f2493e5dea451d6933e8badd2efc862118e
Ilham Nur <romdhoninuril@gmail.com>
2022-08-17 12:56:24 +0700

    First custom

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/blog-single', function () {
+    return view('blog-single');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/schedule', function () {
+    return view('schedule');
+});
+
+Route::get('/speakers', function () {
+    return view('speakers');
+});
